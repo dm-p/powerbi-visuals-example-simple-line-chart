76e01767bb2f206e12dafb0e4ec4b959a84c4162
Daniel Marsh-Patrick <d.s.marsh@gmail.com>
2019-08-13 11:05:36 +1200

    All changes to complete the visual for part 1 of the tutorial.

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # powerbi-visuals-example-simple-line-chart
-Very simple demonstration of migrating a regular d3.js line chart into the Power BI Custom Visuals SDK
+
+This is a very simple demonstration of migrating a regular d3.js line chart into the Power BI Custom Visuals SDK, and is documented in these online posts:
+
+* [Lifting and Shifting a Simple d3.js Line Chart into A Power BI Custom Visual (Part 1)](https://coacervo.co/examples/d3-line-chart-1)
+* Lifting and Shifting a Simple d3.js Line Chart into A Power BI Custom Visual (Part 2) - TBC
+
+Please refer to these articles for further details.
+
+To work with this repository, you will need the Power BI Custom Visuals SDK installed. [Refer here for instructions on setting up your developer environment](https://docs.microsoft.com/en-us/power-bi/developer/custom-visual-develop-tutorial#setting-up-the-developer-environment).
+
+Once checked out, and the pre-requisites set up, you just need to run:
+
+```
+npm i
+```
+
+From the root of the checkout, and then:
+
+```
+pbiviz start
+```
+
+To run the visual server. You can then test in the Power BI service.
---
+++ src/visual.ts
@@ -0,0 +1,117 @@
+/*
+ *  Simple d3.js Line Chart
+ *
+ *  Copyright (c) Daniel Marsh-Patrick
+ *  All rights reserved.
+ *  MIT License
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the ""Software""), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+
+"use strict";
+
+import "core-js/stable";
+import "./../style/visual.less";
+import powerbi from "powerbi-visuals-api";
+import VisualConstructorOptions = powerbi.extensibility.visual.VisualConstructorOptions;
+import VisualUpdateOptions = powerbi.extensibility.visual.VisualUpdateOptions;
+import IVisual = powerbi.extensibility.visual.IVisual;
+import EnumerateVisualObjectInstancesOptions = powerbi.EnumerateVisualObjectInstancesOptions;
+import VisualObjectInstance = powerbi.VisualObjectInstance;
+import DataView = powerbi.DataView;
+import VisualObjectInstanceEnumerationObject = powerbi.VisualObjectInstanceEnumerationObject;
+import { VisualSettings } from "./settings";
+
+/** This specifices the 'shape' of the data in each row. */
+    interface ILineChartRow {
+        date: Date,
+        value: number
+    }
+
+export class Visual implements IVisual {
+    private target: HTMLElement;
+    private settings: VisualSettings;
+
+    constructor(options: VisualConstructorOptions) {
+        console.log('Visual constructor', options);
+        this.target = options.element;
+    }
+
+    public update(options: VisualUpdateOptions) {
+        console.log('Visual update', options);
+        this.settings = Visual.parseSettings(options && options.dataViews && options.dataViews[0]);
+        
+        /** Test 1: Data view has both fields added */
+            let dataViews = options.dataViews;
+            console.log('Test 1: Valid data view...');
+            if (!dataViews
+                || !dataViews[0]
+                || !dataViews[0].categorical
+                || !dataViews[0].categorical.categories
+                || !dataViews[0].categorical.values
+                || !dataViews[0].metadata
+            ) {
+                console.log('Test 1 FAILED. No data to draw table.');
+                return;
+            }
+
+        /** If we get this far, we can trust that we can work with the data! */
+            let categorical = dataViews[0].categorical;
+
+        /** Test 2: Category matches our expected data type (dateTime) */
+            console.log('Test 2: Category field has correct type (dateTime)...');
+            if (!categorical.categories[0].source.type.dateTime) {
+                console.log('Test 2 FAILED. Category is incorrect data type.');
+                return;
+            }
+
+        /** Map our data into an array that looks like our reference visual.
+         *  We're going to map the `categories[0].values` array in-place and return
+         *  an `ILineChartRow` object for each entry. Because the values array elements
+         *  correspond with their equivalent `categories[0].values` element, we can use
+         *  the current index to get the value as we map.
+         *  note that Power BI sees values as `PrimitiveType`, which means we need to 
+         *  cast them as the types we need to match the interface we defined. */
+            let data: ILineChartRow[] = categorical.categories[0].values.map(
+                (cat, idx) => (
+                    {
+                        date: <Date>cat,
+                        value: <number>categorical.values[0].values[idx]
+                    }
+                )
+            );
+
+        /** Parse our mapped data and view the output */
+            console.log(data);
+
+    }
+
+    private static parseSettings(dataView: DataView): VisualSettings {
+        return VisualSettings.parse(dataView) as VisualSettings;
+    }
+
+    /**
+     * This function gets called for each of the objects defined in the capabilities files and allows you to select which of the
+     * objects and properties you want to expose to the users in the property pane.
+     *
+     */
+    public enumerateObjectInstances(options: EnumerateVisualObjectInstancesOptions): VisualObjectInstance[] | VisualObjectInstanceEnumerationObject {
+        return VisualSettings.enumerateObjectInstances(this.settings || VisualSettings.getDefault(), options);
+    }
+}
