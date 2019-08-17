558c31402ad9d23f5ff3e2c1ae919fbb2c359e6c
Daniel Marsh-Patrick <d.s.marsh@gmail.com>
2019-08-17 16:43:57 +1200

    Final changes to finish migration of our reference chart

--- src/visual.ts
+++ src/visual.ts
@@ -124,6 +124,32 @@ export class Visual implements IVisual {
                     .attr('transform',
                         'translate(' + margin.left + ',' + margin.top + ')');
 
+        /** Add X axis --> it is a date format */
+            var x = d3.scaleTime()
+                .domain(d3.extent(data, function(d) { return d.date; }))
+                .range([ 0, width ]);
+            svg.append('g')
+                .attr('transform', 'translate(0,' + height + ')')
+                .call(d3.axisBottom(x));
+
+        /** Add Y axis */
+            var y = d3.scaleLinear()
+                .domain([0, d3.max(data, function(d) { return +d.value; })])
+                .range([ height, 0 ]);
+            svg.append('g')
+                .call(d3.axisLeft(y));
+
+        /** Add the line */
+            svg.append('path')
+                .datum(data)
+                    .attr('fill', 'none')
+                    .attr('stroke', 'steelblue')
+                    .attr('stroke-width', 1.5)
+                    .attr('d', d3.line<ILineChartRow>()
+                        .x(function(d) { return x(d.date) })
+                        .y(function(d) { return y(d.value) })
+                    )
+
     }
 
     private static parseSettings(dataView: DataView): VisualSettings {
